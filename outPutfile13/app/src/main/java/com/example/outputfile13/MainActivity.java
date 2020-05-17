f61228d2bc32f4927f5b4d726f2e3c07a8d5d6ba
Razaul_islam <1709346978@qq.com>
2020-05-18 02:34:19 +0800

    fileOutput

--- outPutfile13/app/src/main/java/com/example/outputfile13/MainActivity.java
+++ outPutfile13/app/src/main/java/com/example/outputfile13/MainActivity.java
@@ -7,19 +7,43 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     EditText open_ETTV;
-    Button btn_OpAC;
+    Button btn_OpAC, btn_InPAC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialization();
         btn_OpAC();
+        btn_InPAC();
+
+    }
+
+    private void btn_InPAC() {
+        btn_InPAC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileInputStream fos =openFileInput("WallCometoNiceWorldRead.txt");
+                    byte[] content_byte =new byte[fos.available()];
+                    fos.read(content_byte);
+                    String content = new String(content_byte);
+                    open_ETTV.setText(content);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
     private void btn_OpAC() {
@@ -27,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    FileOutputStream fos = openFileOutput("WallCometoNiceWorld.txt",MODE_PRIVATE);
+                    FileOutputStream fos = openFileOutput("WallCometoNiceWorldRead.txt",MODE_PRIVATE);
                     fos.write(open_ETTV.getText().toString().getBytes());
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
@@ -41,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
     private void initialization() {
         open_ETTV =findViewById(R.id.open_ETTV);
         btn_OpAC =findViewById(R.id.btn_OpAC);
+        btn_InPAC = findViewById(R.id.btn_InPAC);
+
     }
 }
 
